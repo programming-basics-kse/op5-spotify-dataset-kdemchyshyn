76739ad252e1b280ce245b7f94192f5561c2d6ac
kdemchyshyn <kdemchyshyn@kse.org.ua>
2024-11-04 16:37:42 +0200

    task 1, 2, 3

--- analyze_spotify.py
+++ analyze_spotify.py
@@ -30,6 +30,15 @@ for line in lines:
 
 danceability_avg = danceability_sum / len(lines)
 liveness_avg = liveness_sum / count
-top_three = sorted(artist_tracks.items(), key = lambda x: x[1], reverse=True)
 
-print(danceability_avg, liveness_avg, top_three[:3])
+artists_sorted = sorted(artist_tracks.items(), key = lambda x: x[1], reverse=True)
+artist_top_num_tracks = []
+artist_top = []
+for artist in artists_sorted:
+    if len(artist_top_num_tracks) < 3 and artist[1] not in artist_top_num_tracks:
+        artist_top_num_tracks.append(artist[1])
+        artist_top.append(artist[0])
+    elif artist[1] in artist_top_num_tracks:
+        artist_top.append(artist[0])
+
+print(danceability_avg, liveness_avg, artist_top)
