2f3b8b0ee13a976c9e1a8ee991aef531e5ef21a7
kdemchyshyn <kdemchyshyn@kse.org.ua>
2024-11-05 11:21:18 +0200

    task 1, 2, 3, 5

--- analyze_spotify.py
+++ analyze_spotify.py
@@ -1,5 +1,10 @@
 import csv
 
+def is_in_dict(key, dictionary):
+    if key not in dictionary:
+        return 1
+    return dictionary[key] + 1
+
 lines = []
 with open("top_50_2023.csv", 'r') as file:
     csv_reader = csv.reader(file, delimiter=',')
@@ -11,22 +16,24 @@ danceability = header.index("danceability")
 liveness = header.index("liveness")
 energy = header.index("energy")
 artist_name = header.index("artist_name")
+album_release_date = header.index("album_release_date")
 
 danceability_sum = 0
 liveness_sum = 0
 count = 0
 
+album_release_year = {}
 artist_tracks = {}
 
 for line in lines:
     danceability_sum += float(line[danceability])
+
     if float(line[energy]) > 0.5:
         liveness_sum += float(line[liveness])
         count += 1
-    if line[artist_name] not in artist_tracks:
-        artist_tracks[line[artist_name]] = 1
-    else:
-        artist_tracks[line[artist_name]] += 1
+
+    artist_tracks[line[artist_name]] = is_in_dict(line[artist_name], artist_tracks)
+    album_release_year[line[album_release_date][:4]] = is_in_dict(line[album_release_date][:4], album_release_year)
 
 danceability_avg = danceability_sum / len(lines)
 liveness_avg = liveness_sum / count
@@ -41,4 +48,7 @@ for artist in artists_sorted:
     elif artist[1] in artist_top_num_tracks:
         artist_top.append(artist[0])
 
-print(danceability_avg, liveness_avg, artist_top)
+top_year_index = [value for key, value in album_release_year.items()].index(max([value for key, value in album_release_year.items()]))
+top_year = [key for key, value in album_release_year.items()][top_year_index]
+
+print(f"{danceability_avg}\n{liveness_avg}\n{artist_top}\n{top_year}")
